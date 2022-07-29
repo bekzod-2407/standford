064b0d1de574c22da4f8f4c13653dd098b475f55
Bekzod <bekzod.k2407@gmail.com>
2022-07-29 08:47:45 +0500

    added file playingCards

---
+++ standford/SupportingFiles/PlayingCards.swift
@@ -0,0 +1,38 @@
+//
+//  PlayingCards.swift
+//  standford
+//
+//  Created by Bekzod Qahhorov on 29/07/22.
+//
+
+import Foundation
+
+struct PlayingCard {
+    
+    var suit: Suit
+    var rank: Rank
+    
+    enum Suit:String {
+        case spades =  "♤"
+        case heart = "♡"
+        case clubs = "♧"
+        case diamonds = "♢"
+    }
+    
+    enum Rank {
+        case ace
+        case fase(String)
+        case numeric(Int)
+        
+        var order: Int {
+            switch self {
+            case .ace: return 1
+            case .fase(let king) where king == "J" : return 11
+            case .fase(let king) where king == "Q" : return 12
+            case .fase(let king) where king == "K" : return 13
+            case .numeric(let pips): return pips
+            default: return 0
+            }
+        }
+    }
+}
